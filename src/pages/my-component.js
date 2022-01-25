028da4f9ac988b160fb98d0102d579f612475a8b
Jonathan Kane <jon.kane@solidstatescientific.com>
2022-01-25 18:22:23 +0000

    trying to get this to update

---
+++ src/pages/my-component.js
@@ -0,0 +1,12 @@
+//step 1: Import React. This lets you use JSX inside your .js file
+import * as React from 'react'
+
+/* Step 2: Define your component. Note that your component name should startiwth a capital letter */
+const Mycomponent = () => {
+    return (
+        <h1>Hi, Wecome to my Site!</h1>
+    )
+}
+
+/* Step 3: Export your components so it can used by other parts of your app */
+export default Mycomponent
