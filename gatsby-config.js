76b167bdabff43cf6842a0590740bcc1adcc48b4
jskane <65679702+jskane@users.noreply.github.com>
2022-02-08 16:10:30 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,3 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-west-2_lFiIm1rbN', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3veja9k7dt4lu89rtrol8tia0r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
-
 module.exports = {
   siteMetadata: {
     title: "My Super Cool Blog",
