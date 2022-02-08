f07a88a83ba95553abdb996b7aef4235d2aed959
jskane <65679702+jskane@users.noreply.github.com>
2022-02-08 16:21:27 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
-import Amplify from 'aws-amplify'
-import config from '../aws-exports'
-Amplify.configure(config)
+
 
 const IndexPage = () => {
   return (
@@ -12,10 +10,6 @@ const IndexPage = () => {
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
-      <Link to="/app/login">Sign In</Link><br />
-      <Link to="/app/home">Home</Link><br />
-      <Link to="/app/profile">Your profile</Link>
       />
     </Layout>
   )
